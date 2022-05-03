5dcea3e2ba4975394ff4692202818ca040787f45
popo26 <mynameispopo26@gmail.com>
2022-05-04 07:41:42 +1200

    updated ALLOWED_HOSTS with site and development server URL

--- heroku/settings.py
+++ heroku/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = os.getenv("DEBUG", '') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['obscure-mesa-48623.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
